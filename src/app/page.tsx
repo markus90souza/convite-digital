5caa4696d83ce8dc212b6e233c66a76a55cd3419
rockske8er <rockske8er@gmail.com>
2024-11-18 13:46:47 -0300

    feat:finish lesson 01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Container } from '@/components/template/container'
 
 export default function Home() {
   return (
-    <Container className="bg-red-500 w-full">
+    <Container className="w-full">
       <div className="">inicio</div>
     </Container>
   )
